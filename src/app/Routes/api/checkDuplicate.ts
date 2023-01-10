50a5460af75fd1bdd415c34e7e7155bc0266142c
ndollawa <ndollawa@yahoo.com>
2023-01-10 04:27:43 -0800

    Paybuddie Backend

--- src/app/Routes/api/checkDuplicate.ts
+++ src/app/Routes/api/checkDuplicate.ts
@@ -1,9 +1,9 @@
 import  express,{Response,Request,NextFunction} from 'express';
 const router = express.Router();
 import path from 'path';
-import AuthController from '../../Http/Controllers/AuthController';
+import checkDuplicate from '../../Http/Controllers/DuplicateController';
 
 router.route('/')
-.post((req:Request, res:Response,next:NextFunction)=>AuthController.checkDuplicate)
+.post((req:Request, res:Response,next:NextFunction)=>checkDuplicate(req,res))
 
 export default router;  
